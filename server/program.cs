798a2c65bd59603e520e5d7b7ba3cdfdfe4da189
nataries <natalizastavnaya@icloud.com>
2026-02-01 20:49:30 +0300

    Added logic of choice program.cs

--- server/program.cs
+++ server/program.cs
@@ -10,5 +10,25 @@ while (true)
     Console.WriteLine("3 - Показать дату");
     Console.WriteLine("4 - Выход");
     Console.Write("Ваш выбор (введите цифру от 1 до 4): ");
-   
+    string? choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            Console.WriteLine($"ФИО - {myName}");
+            break;
+
+        case "2":
+            Console.WriteLine($"Группа - {group}");
+            break;
+        case "3":
+            Console.WriteLine($"Текущая дата: {curDate}");
+            break;
+        case "4":
+            Console.WriteLine("Вы вышли из программы!");
+            return;
+        default:
+            Console.WriteLine("Ошибка, такого варианта нет");
+            break;
+    }
 }
